1479023e69101eca3e6009e2909f37185cbc96b3
tie <>
2025-09-16 13:02:31 +0200

    replace " with ' in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,9 +3,9 @@ vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
 vim.o.cursorline = true
-vim.o.cursorlineopt = "number"
+vim.o.cursorlineopt = 'number'
 vim.o.list = true
-vim.o.listchars = "trail:·"
+vim.o.listchars = 'trail:·'
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.scrolloff = 4
@@ -21,39 +21,39 @@ vim.o.undofile = true
 vim.o.title = true
 
 vim.g.netrw_banner = false
-vim.g.netrw_bufsettings = "nu rnu"
+vim.g.netrw_bufsettings = 'nu rnu'
 
-vim.g.mapleader = " "
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
-vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
-vim.keymap.set("n", "<leader>e", "<cmd>Explore<CR>")
-vim.keymap.set("n", "<leader>m", "<cmd>write | make<CR>")
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>")
+vim.g.mapleader = ' '
+vim.keymap.set({ 'n', 'v' }, '<leader>y', "'+y")
+vim.keymap.set({ 'n', 'v' }, '<leader>p', "'+p")
+vim.keymap.set('n', '<leader>e', '<cmd>Explore<CR>')
+vim.keymap.set('n', '<leader>m', '<cmd>write | make<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
     callback = function()
         vim.hl.on_yank()
     end,
 })
 
-require("paq") {
-    "savq/paq-nvim",
-    "windwp/nvim-autopairs",
-    "norcalli/nvim-colorizer.lua",
-    "rebelot/kanagawa.nvim",
+require('paq') {
+    'savq/paq-nvim',
+    'windwp/nvim-autopairs',
+    'norcalli/nvim-colorizer.lua',
+    'rebelot/kanagawa.nvim',
 }
 
-require("nvim-autopairs").setup()
-require("colorizer").setup()
+require('nvim-autopairs').setup()
+require('colorizer').setup()
 
-require("kanagawa").setup({
+require('kanagawa').setup({
     commentStyle = { italic = false },
     keywordStyle = { italic = false },
 })
-vim.cmd("colorscheme kanagawa-dragon")
-vim.cmd("highlight LineNr guibg=None")
-vim.cmd("highlight CursorLineNr guibg=None")
-vim.cmd("highlight SignColumn guibg=None")
-vim.cmd("highlight FoldColumn guibg=None")
+vim.cmd('colorscheme kanagawa-dragon')
+vim.cmd('highlight LineNr guibg=None')
+vim.cmd('highlight CursorLineNr guibg=None')
+vim.cmd('highlight SignColumn guibg=None')
+vim.cmd('highlight FoldColumn guibg=None')
