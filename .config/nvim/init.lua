62ab38d8e010df2e529ec04b7da5f96f3dd9d11b
tie <extremetie@proton.me>
2025-09-18 22:15:20 +0200

    remove yank highlight + clean up

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,18 @@
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
 vim.o.expandtab = true
-
+vim.o.ignorecase = true
 vim.o.list = true
 vim.o.listchars = 'tab:→ ,trail:·,nbsp:␣'
-
 vim.o.number = true
 vim.o.relativenumber = true
-
-vim.o.ignorecase = true
+vim.o.scrolloff = 4
+vim.o.shiftwidth = 4
 vim.o.smartcase = true
-
 vim.o.splitbelow = true
 vim.o.splitright = true
-
-vim.o.scrolloff = 4
+vim.o.tabstop = 4
 vim.o.termguicolors = true
-vim.o.undofile = true
 vim.o.title = true
+vim.o.undofile = true
 
 vim.g.mapleader = ' '
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
@@ -25,14 +20,6 @@ vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p')
 vim.keymap.set('n', '<leader>e', '<cmd>Oil<CR>')
 vim.keymap.set('n', '<leader>m', '<cmd>write | make<CR>')
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-    callback = function()
-        vim.hl.on_yank()
-    end,
-})
 
 require('paq') {
     'stevearc/oil.nvim',
@@ -47,10 +34,8 @@ require('oil').setup({
     view_options = { show_hidden = true },
 })
 require('mini.icons').setup()
-
 require('nvim-autopairs').setup()
 require('colorizer').setup()
-
 require('kanagawa').setup({
     commentStyle = { italic = false },
     keywordStyle = { italic = false },
